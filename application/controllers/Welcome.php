d49ba522db15c55ef80b3fc910ec3311c855d401
Harold <gngud21@naver.com>
2017-02-16 02:07:45 -0800

    finished job2

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,8 +17,40 @@ class Welcome extends Application
 	 * map to /welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	function render($template = 'template')
+	{
+	    $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
+	    // use layout content if provided
+	    if (!isset($this->data['content']))
+	        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+	    $this->parser->parse($template, $this->data);
+	}
+
 	public function index()
 	{
+
+		$tasks = $this->tasks->all();   // get all the tasks
+
+		// count how many are not done
+		$count = 0;
+		foreach($tasks as $task) {
+		        if ($task->status != 2) $count++;
+		}
+		// and save that as a view parameter
+		$this->data['remaining_tasks'] = $count;
+
+		// process the array in reverse, until we have five
+		$count = 0;
+		foreach(array_reverse($tasks) as $task) {
+			$task->priority = $this->priorities->get($task->priority)->name; //change num to priorities
+		    $display_tasks[] = (array) $task;
+		    $count++;
+		    if ($count >= 5) break;
+		}
+		$this->data['display_tasks'] = $display_tasks;
+
+
 		$this->data['pagebody'] = 'homepage';
 		$this->render(); 
 	}
